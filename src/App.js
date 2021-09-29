d5c4dfc16e002c61985f02aff7556bc8fe5012ff
Kazi Md Ragib <58323531+kaziragib18@users.noreply.github.com>
2021-09-30 00:12:04 +0600

    nav active explored

--- src/App.js
+++ src/App.js
@@ -4,35 +4,39 @@ import Home from './components/Home/Home';
 import Friends from './components/Friends/Friends';
 import About from './components/About/About';
 import NotFound from './components/NotFound/NotFound';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Header from './components/Header/Header';
 
 
 function App() {
   return (
     <div className="App">
-        <Router>
+
+      <Router>
+      <Header></Header>
+      
         <Switch>
 
-         <Route path="/home">
-           <Home></Home>
-         </Route>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
 
-         <Route path="/about">
-           <About></About>
-         </Route>
+          <Route path="/about">
+            <About></About>
+          </Route>
 
-         <Route path="/friends">
-           <Friends></Friends>
-         </Route>
-         <Route exact path="/">
-         </Route>
-        
-         <Route path="*"> 
-           <NotFound></NotFound>
-         </Route>
+          <Route path="/friends">
+            <Friends></Friends>
+          </Route>
+          <Route exact path="/">
+          </Route>
+
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
 
         </Switch>
-        </Router>
+      </Router>
     </div>
   );
 }
